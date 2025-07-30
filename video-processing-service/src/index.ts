1afb62d51d14a9e0e60d3893191e5ea51f84aa3e
Utkarsh-32 <utkarsh.shendre05@gmail.com>
2025-07-30 16:23:09 +0530

    Containerize video processing service

---
+++ video-processing-service/src/index.ts
@@ -0,0 +1,31 @@
+import express from "express"
+import ffmpeg from "fluent-ffmpeg"
+
+const app = express();
+app.use(express.json());
+
+
+app.post("/process-video", (req, res) => {
+    const inputFilePath = req.body.inputFilePath;
+    const outputFilePath = req.body.outputFilePath;
+
+    if (!inputFilePath || !outputFilePath) {
+        res.status(400).send("Missing input or output file path")
+    }
+
+    ffmpeg(inputFilePath)
+      .outputOptions("-vf", "scale=-1:720")
+      .on("end", () => {
+        res.status(200).send("Video processing finished successfully")
+      })
+      .on("error", (err) => {
+        console.log(`An error has occurred: ${err.message}`);
+        res.status(500).send(`Internal server errror: ${err.message}`)
+      })
+      .save(outputFilePath)
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port , () => {
+    console.log(`Video processing service listening at http://localhost:${port}`);
+});
