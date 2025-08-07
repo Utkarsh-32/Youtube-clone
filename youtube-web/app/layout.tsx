82734149e9f4245ee91fd9914a9a8bfc456fc620
Utkarsh-32 <utkarsh.shendre05@gmail.com>
2025-08-07 17:04:04 +0530

    Add navbar

---
+++ youtube-web/app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import Navbar from "./navbar/navbar";
+
+
+
+export const metadata: Metadata = {
+  title: "YouTube",
+  description: "Youtube clone",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        <Navbar />
+        {children}
+      </body>
+    </html>
+  );
+}
